e7b8d6feae7760a14671c5072acfca023f68c8ac
netlesh <netlesh@mail.ru>
2022-07-05 16:39:39 +0300

    curriculum

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -21,36 +21,40 @@ module.exports = {
         text: "Grammar",
         children: [
           {
-            text: "Structure",
+            text: "Sentence Structure",
             children: ["/02_grammar/01_structure/01_text.md", "/02_grammar/01_structure/02_unicode.md", "/02_grammar/01_structure/03_semicolons.md"],
           },
           {
-            text: "Types, Values, and Variables",
-            children: ["/02_grammar/02_types_values_variables/01_types_symbols.md", "/02_grammar/02_types_values_variables/02_mutable_immutable.md", "/02_grammar/02_types_values_variables/03_type_conversions_variables.md"],
+            text: "Words",
+            children: ["/02_grammar/02_words/01_types_symbols.md", "/02_grammar/02_words/02_mutable_immutable.md", "/02_grammar/02_words/03_type_conversions_variables.md"],
           },
           {
-            text: "Expressions and Operators",
+            text: "Parts of Language",
             children: ["/02_grammar/03_expressions_operators/01_expressions.md", "/02_grammar/03_expressions_operators/02_access_expression.md", "/02_grammar/03_expressions_operators/03_operators.md"],
           },
           {
             text: "Statements",
-            children: ["/01_meet_again/01-what-is.md"],
+            children: ["/02_grammar/04_statements/01_expression_statements.md", "/02_grammar/04_statements/02_conditionals.md", "/02_grammar/04_statements/03_loops.md", "/02_grammar/04_statements/04_jumps.md"],
           },
           {
             text: "Objects",
-            children: ["/01_meet_again/01-what-is.md"],
+            children: ["/02_grammar/05_objects/01_create_query_set_objects.md", "/02_grammar/05_objects/02_delete_test_enumerate_objects.md", "/02_grammar/05_objects/03_extend_serialize_objects.md", "/02_grammar/05_objects/04_object_methods.md", "/02_grammar/05_objects/05_extended_object_literal_syntax.md"],
           },
           {
-            text: "Functions",
-            children: ["/01_meet_again/01-what-is.md"],
+            text: "Nouns , Pronouns and Adjectives",
+            children: ["/02_grammar/06_collections/01_crud_arrays.md", "/02_grammar/06_collections/02_iterating_arrays", "/02_grammar/06_collections/03_array_methods", "/02_grammar/06_collections/04_objects_like_array", "/02_grammar/06_collections/05_maps"],
           },
           {
-            text: "Classes",
-            children: ["/01_meet_again/01-what-is.md"],
+            text: "Verbs",
+            children: ["/02_grammar/07_functions/01_functions", "/02_grammar/07_functions/02_arguments_parameters", "/02_grammar/07_functions/03_closures"],
           },
           {
-            text: "Modules",
-            children: ["/01_meet_again/01-what-is.md"],
+            text: "Clauses",
+            children: ["/02_grammar/08_classes/01_classes", "/02_grammar/08_classes/02_extend", "/02_grammar/08_classes/03_mixins"],
+          },
+          {
+            text: "Tenses",
+            children: ["/02_grammar/09_asynchronous/01_asynchronous", "/02_grammar/09_asynchronous/02_promises", "/02_grammar/09_asynchronous/03_async_await"],
           },
         ],
       },
@@ -58,28 +62,42 @@ module.exports = {
         text: "Literature",
         children: [
           {
-            text: "Organization",
-            children: ["/03_javascript_fundamentals/01-what-is.md", "/01_meet_again/02-history.md"],
-          },
-          {
-            text: "Async Flow Control",
-            children: ["/03_javascript_fundamentals/01-what-is.md", "/01_meet_again/02-history.md"],
-          },
-          {
-            text: "Collections",
-            children: ["/03_javascript_fundamentals/01-what-is.md", "/01_meet_again/02-history.md"],
+            text: "Organization by Chapters",
+            children: [
+              // "Modules"
+            ],
           },
           {
             text: "Meta Programming",
-            children: ["/03_javascript_fundamentals/01-what-is.md", "/01_meet_again/02-history.md"],
+            children: [
+              // "Proxies", "Reflect API"
+            ],
+          },
+          {
+            text: "Object Oriented Programming",
+            children: [],
+          },
+          {
+            text: "Functional Programming",
+            children: [],
           },
           {
             text: "Browser",
-            children: ["/03_javascript_fundamentals/01-what-is.md", "/01_meet_again/02-history.md"],
+            children: [
+              // "Document", "Events", "Interfaces", "Memory", "Network", "Animation", "Regex"
+            ],
           },
           {
             text: "Node",
-            children: ["/03_javascript_fundamentals/01-what-is.md", "/01_meet_again/02-history.md"],
+            children: [],
+          },
+          {
+            text: "TypeScript ",
+            children: [],
+          },
+          {
+            text: "Deno",
+            children: [],
           },
         ],
       },
--- 00_method_introduction/index.md
+++ 00_method_introduction/index.md
@@ -1,5 +1,9 @@
 # Re-Beginners Method
 
+Deep dive into the fundamentals of JavaScript
+
+Human knowledge is expressed in language so while developing Re-Beginners Method; **I tried to understand software languages from a written and spoken language perspective**.
+
 Back to Basics of Coding with ...
 ![Cem Kaan](./profile.png)
 
@@ -10,24 +14,28 @@ But last 5 years, I have noticed there were strange technological developments i
 
 So I decided to start over from beginning :man_technologist: I've **re-learned** everything about javascript. I prepared this course to share my experience in this re-learning process. :world_map: <FontIcon icon="route" size="23" color="Crimson"/>
 
-## For Whom Was This Course Is Prepeared?
+## For Whom Was This Course Is Prepared?
+
+### 70 % of developers are second-language speakers of English
 
 Check if this course for you before you start climbing  :mount_fuji:
 
-Did you try to self-teach programming from _free youtube videos_, but you couldn't seem to stick with it? Do you think that something is missing? Then, how about re-learning it step-by-step? Whatever you think you already know can be fast-forwarded with this step-by-step approach.
+Did you try to self-teach programming from _free youtube videos_, but you couldn't seem to stick with it? Do you think that something is missing? Then, how about re-learning it like Learning a spoken language? Whatever you think you already know can be fast-forwarded with this Linguistic approach.
 
 **JavaScript for re-beginners** is for young software students, who wants to have a strong background and interested in a complete javascript guide.
 
 I want to share my 20 years of expertise to build a strong fundamental background your your software development career.  
 
-## Step-by-step Approuch
+## Learning Language by Linguistic Approach
 
 I believe understanding how languages works is important. That's why I spend so much time on organizing this course.
 It's intended for programmers who have been working with JavaScript at a beginner level and are now ready for a more sophisticated relationship with the language. JavaScript is a surprisingly powerful but most misunderstood language.
 May be you had some coding lessons previously but feeling like you’ve missed some basics.
-This step-by-step approuch is probably for you.
+This Linguistic approach is probably for you.
 I will explain low-level fundamentals first, and then build on those to more
 advanced and higher-level concepts
+
+I will dive deeper while explaining hard to understand concepts but spend less time on basics.
 
 Once you learn syntax of JavaScript which is similar to Java language, you will be able to learn  Typescript which is a strict data typing extension of javascript.
 Next you can even learn some modern languages like Dart and Kotlin which has similar syntax too.
